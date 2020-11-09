a838487f3b444e77c5145d253c1ede6725727e79
Ronald Tse <ronald.tse@ribose.com>
2020-11-10 01:27:21 +0800

    Update metadata of new guides and update structure

--- its-90_bilingual.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-bundle exec metanorma -t bipm -x xml sources/guide-its-90/eit90-fr.adoc
-bundle exec metanorma -t bipm -x xml sources/guide-its-90/its90-en.adoc
-bundle exec metanorma collection sources/guide-its-90/its-90-bilingual.yml -x xml,html,presentation,pdf -w bilingual_its-90 -c sources/guide-its-90/collection_cover.html
---
+++ sources/brochure-its90/compose.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+bundle exec metanorma -x xml brochure-its90-fr.adoc
+bundle exec metanorma -x xml brochure-its90-en.adoc
+bundle exec metanorma collection brochure-its90-bilingual.yml -x xml,html,presentation,pdf -w brochure-its90-bilingual -c collection_cover.html
